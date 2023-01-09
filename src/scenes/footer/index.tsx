331245e064f5de391f43791351a43454282dfd68
Pham Nhat Tien <nhattien.ee@gmail.com>
2023-01-09 15:13:10 +0700

    first commit

---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,32 @@
+import Logo2 from "@/assets/Logo2.png";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img className="h-12 w-40" alt="logo" src={Logo2} />
+          <p className="my-5">
+            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
+            purus et arcu massa dictum condimentum. Augue scelerisque iaculis
+            orci ut habitant laoreet. Iaculis tristique.
+          </p>
+          <p>© TienPham Fitness All Rights Reserved.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Lorem vitae ut</p>
+          <p className="my-5">augue auctor faucibus </p>
+          <p>Iaculis tristique</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
+          <p>(84)96-527-3494</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
